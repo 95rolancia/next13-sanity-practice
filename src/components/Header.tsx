8e753300fe894f2ba605ccf22ec187f9e0b6b4d3
jjangjun <95jjangjun@gmail.com>
2023-10-23 21:21:24 +0900

    style: layout, header style 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <header className="flex justify-between p-4 border-2 border-sky-400">
+    <header className="flex justify-between p-4 border-b border-sky-400 sticky top-0 bg-white z-10">
       <Link href="/">
         <h1 className="text-2xl font-bold">Instantgram</h1>
       </Link>
