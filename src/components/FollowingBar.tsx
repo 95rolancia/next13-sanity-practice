36e6050885620240b806848dfe4a45e9713ce9d9
jjangjun <95jjangjun@gmail.com>
2023-11-18 13:54:20 +0900

    feat: 팔로잉바 구현

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,3 +1,41 @@
+"use client";
+
+import useSWR from "swr";
+import Avatar from "./Avatar";
+import { DetailUser } from "@/model/user";
+import { PropagateLoader } from "react-spinners";
+import Link from "next/link";
+import ScrollableBar from "./ui/ScrollableBar";
+
 export default function FollowingBar() {
-  return <h1>FollowingBar</h1>;
+  const { data, isLoading } = useSWR<DetailUser>("/api/me");
+
+  const users = data?.following;
+
+  return (
+    <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[124px] overflow-x-auto">
+      {isLoading ? (
+        <PropagateLoader size={8} color="red" />
+      ) : (
+        (!users || users.length === 0) && <p>{"You dont have followings"}</p>
+      )}
+
+      {users && users.length > 0 && (
+        <ScrollableBar>
+          {users.map(({ username, image }) => (
+            <Link
+              key={username}
+              href={`/user/${username}`}
+              className="flex flex-col items-center w-20"
+            >
+              <Avatar image={image} highlight />
+              <p className="w-full text-center text-ellipsis overflow-hidden">
+                {username}
+              </p>
+            </Link>
+          ))}
+        </ScrollableBar>
+      )}
+    </section>
+  );
 }
