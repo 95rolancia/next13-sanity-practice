94cb2e1a39cabdd6e8725ffabcfdb7fb50ddd971
jjangjun <95jjangjun@gmail.com>
2023-11-18 13:51:02 +0900

    fix: dataset 수정

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 export const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANIFY_DATASET,
+  dataset: process.env.SANITY_DATASET,
   apiVersion: "2023-10-31",
   useCdn: false,
   token: process.env.SANITY_SECRET_TOKEN || "",
