0a2619298da70d5d36b6176c057309a802ae0b3c
jjangjun <95jjangjun@gmail.com>
2024-01-04 22:09:44 +0900

    feat: 사용자 검색

---
+++ src/components/UserSearch.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { ProfileUser } from "@/model/user";
+import { FormEvent, useState } from "react";
+import { GridLoader } from "react-spinners";
+import useSWR from "swr";
+import UserCard from "./UserCard";
+
+export default function UserSearch() {
+  // /api/search/${keyword}
+  // 검색하는 키워드가 있다면 /api/search/bob -> Username이나, 네임에 있다면
+  // 검색하는 키워드가 없다면 /api/search -> 전체 유저
+
+  const [keyword, setKeyword] = useState("");
+
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useSWR<ProfileUser[]>(`/api/search/${keyword}`);
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="w-full max-w-2xl my-4 flex flex-col items-center">
+      <form className="w-full mb-4" onSubmit={onSubmit}>
+        <input
+          className="w-full text-xl p-3 outline-none border border-gray-400"
+          type="text"
+          autoFocus
+          placeholder="Search for a username or name"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </form>
+      {error && <p>Error!</p>}
+      {isLoading && <GridLoader />}
+
+      {!isLoading && !error && users?.length === 0 && <p>찾는 사용자가 없음</p>}
+
+      <ul className="w-full p-4">
+        {users &&
+          users.map((user) => (
+            <li key={user.username}>
+              <UserCard user={user} />
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
+}
