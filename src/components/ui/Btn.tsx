0406f816e6dc9936bcd9f9334d4888ea9918bf13
jjangjun <95jjangjun@gmail.com>
2024-01-07 20:45:51 +0900

    ui: button component

---
+++ src/components/ui/Btn.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+type Props = {
+  text: string;
+  onClick: () => void;
+  red?: boolean;
+};
+
+export default function Btn({ text, onClick, red }: Props) {
+  return (
+    <button
+      className={`border-none rounded-md py-2 px-8 text-white font-bold leading-4 ${
+        red ? "bg-red-500" : "bg-sky-500"
+      }`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
