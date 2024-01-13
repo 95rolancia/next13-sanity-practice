d78ef47ec308e33fbc03b10bab0ea8676f245842
jjangjun <95jjangjun@gmail.com>
2024-01-13 13:40:09 +0900

    feat: AuthUser에 id 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -10,16 +10,23 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async session({ session }) {
+    async session({ session, token }) {
       const user = session.user;
       if (user) {
         session.user = {
           ...user,
           username: user.email?.split("@")[0] || "",
+          id: token.id as string,
         };
       }
 
       return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
     },
     async signIn({ user: { id, name, image, email } }) {
       if (!email) return false;
