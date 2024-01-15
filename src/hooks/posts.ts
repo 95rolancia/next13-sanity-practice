6a8f571029ccbaef2317a45f3287b0c323a49eed
jjangjun <95jjangjun@gmail.com>
2024-01-15 19:39:53 +0900

    hook: usePosts에 addComment 추가

--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -1,10 +1,17 @@
-import { SimplePost } from "@/model/post";
-import useSWR, { useSWRConfig } from "swr";
+import { Comment, SimplePost } from "@/model/post";
+import useSWR from "swr";
 
 async function updateLike(id: string, like: boolean) {
   return fetch("/api/likes", {
     method: "PUT",
     body: JSON.stringify({ id, like }),
+  }).then((res) => res.json());
+}
+
+async function addComment(id: string, comment: string) {
+  return fetch("/api/posts", {
+    method: "POST",
+    body: JSON.stringify({ id, comment }),
   }).then((res) => res.json());
 }
 
@@ -16,7 +23,7 @@ export default function usePosts() {
     mutate,
   } = useSWR<SimplePost[]>("/api/posts");
 
-  const setLike = async (post: SimplePost, username: string, like: boolean) => {
+  const setLike = (post: SimplePost, username: string, like: boolean) => {
     const newPost = {
       ...post,
       likes: like
@@ -34,5 +41,21 @@ export default function usePosts() {
     });
   };
 
-  return { posts, isLoading, error, setLike };
+  const postComment = (post: SimplePost, comment: Comment) => {
+    const newPost = {
+      ...post,
+      comments: post.comments + 1,
+    };
+
+    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+
+    return mutate(addComment(post.id, comment.comment), {
+      optimisticData: newPosts,
+      populateCache: false,
+      revalidate: false,
+      rollbackOnError: true,
+    });
+  };
+
+  return { posts, isLoading, error, setLike, postComment };
 }
