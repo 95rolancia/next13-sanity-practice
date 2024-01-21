d138261dd16c5b6ff7b520b7b7e16148dc51b0c8
jjangjun <95jjangjun@gmail.com>
2024-01-21 21:27:29 +0900

    feat: 팔로우/언팔 시 현재페이지 리프레쉬 및 리프레쉬 중 버튼 로딩 표현

--- src/components/FollowBtn.tsx
+++ src/components/FollowBtn.tsx
@@ -3,6 +3,9 @@
 import { ProfileUser } from "@/model/user";
 import Btn from "./ui/Btn";
 import useMe from "@/hooks/me";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import { PulseLoader } from "react-spinners";
 
 type Props = {
   user: ProfileUser;
@@ -12,26 +15,42 @@ export default function FollowBtn({ user }: Props) {
   const { username } = user;
   const { user: loggedInUser, toggleFollow } = useMe();
 
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const isUpdating = isPending || isFetching;
+
   const showBtn = loggedInUser && loggedInUser.username !== username;
   const following = !!(
     loggedInUser &&
     loggedInUser.following.find((item) => item.username === username)
   );
 
-  console.log(loggedInUser);
-
   const text = following ? "팔로우 취소" : "팔로우";
 
   return (
     <>
       {showBtn && (
-        <Btn
-          text={text}
-          onClick={() => {
-            toggleFollow(user.id, following);
-          }}
-          red={following}
-        />
+        <div className="relative">
+          {isUpdating && (
+            <div className="z-20 absolute inset-0 flex justify-center items-center">
+              <PulseLoader size={6} color="white" />
+            </div>
+          )}
+          <Btn
+            disabled={isUpdating}
+            text={text}
+            onClick={async () => {
+              setIsFetching(true);
+              await toggleFollow(user.id, following);
+              setIsFetching(false);
+              startTransition(() => {
+                router.refresh();
+              });
+            }}
+            red={following}
+          />
+        </div>
       )}
     </>
   );
