384c9a63188616d16df5ae4b96bb34358a42840a
jjangjun <95jjangjun@gmail.com>
2024-01-22 21:45:41 +0900

    page: 새로운 글쓰기 페이지(ui만)

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,20 @@
-export default function New() {
-  return <>New Post Page</>;
+import NewPost from "@/components/NewPost";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+
+export const metadata: Metadata = {
+  title: "새로운 글",
+  description: "새로운 글 작성하기",
+};
+
+export default async function NewPostPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    redirect("/auth/signin");
+  }
+  
+  return <NewPost user={session.user} />;
 }
